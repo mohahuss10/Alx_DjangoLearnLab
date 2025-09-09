6cc42287fcf47bdab66ccad9da1926f5929234e9
mohamed <mohamedhussein6500@gmail.com>
2025-09-09 22:52:44 +0300

    Fix Task 1: updated imports for DetailView and Library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView 
 from .models import Book
-from .models import Library   # ✅ required by checker
+from .models import Library   
 
-
-# Function-based view: list all books (must use template)
+# Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
