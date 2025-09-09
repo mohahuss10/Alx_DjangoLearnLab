7ec7f72cef708928b101451465123bfb5b177b4c
mohamed <mohamedhussein6500@gmail.com>
2025-09-09 23:20:03 +0300

    Updated urls.py to use built-in LoginView and LogoutView with template_name, fixed auth routing

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from .views import register, CustomLoginView, CustomLogoutView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register, list_books, LibraryDetailView
 
 urlpatterns = [
-    path("books/", list_books, name="list_books"),
-    path("libraries/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-
-    # Authentication URLs
     path("register/", register, name="register"),
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+    path("books/", list_books, name="list_books"),
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 ]
