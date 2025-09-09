d430e6d1bc54536eacdf8d0c2be73c16d75c432b
mohamed <mohamedhussein6500@gmail.com>
2025-09-09 23:30:49 +0300

    Add role-based access control views (Admin, Librarian, Member)

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic.detail import DetailView 
+from django.shortcuts import render, redirect
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import user_passes_test
 
-from .models import Book
-from .models import Library   
+from .models import Book, Library
+
 
 # Function-based view: list all books
 def list_books(request):
@@ -18,10 +19,6 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
-from django.contrib.auth.views import LoginView, LogoutView
 
 
 # Register view
@@ -45,3 +42,29 @@ class CustomLoginView(LoginView):
 # Logout view (class-based)
 class CustomLogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
+
+
+# --- ROLE-BASED ACCESS VIEWS ---
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
