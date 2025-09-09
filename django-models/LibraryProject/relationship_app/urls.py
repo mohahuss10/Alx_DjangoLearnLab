ab8c302f42d93c27f900f5ea82743a41d04fe794
mohamed <mohamedhussein6500@gmail.com>
2025-09-09 23:35:34 +0300

    Add role-based views and URLs for Admin, Librarian, Member

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("books/", views.list_books, name="list_books"),
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+
+    # Role-based access URLs
+    path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view"),
 ]
